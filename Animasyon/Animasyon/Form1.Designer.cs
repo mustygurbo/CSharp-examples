047581008db796bfeedac1eb9775e66e5aa067d7
mustygurbo <57802241+mustygurbo@users.noreply.github.com>
2020-01-22 01:45:08 +0300

    Add files via upload

---
+++ Animasyon/Animasyon/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Animasyon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Animasyon.Properties.Resources._1111;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Animasyon/Animasyon/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Animasyon
+{
+    public partial class Form1 : Form
+    {
+
+        int sira;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (sira == 1)
+            {
+                pictureBox1.Left = pictureBox1.Left + 15;
+                pictureBox1.Image = Properties.Resources._1111;
+                sira += 1;
+            }
+            else if (sira == 2)
+            {
+                pictureBox1.Left = pictureBox1.Left + 15;
+                pictureBox1.Image = Properties.Resources._2222;
+                sira += 1;
+            }
+            else if (sira == 3)
+            {
+                pictureBox1.Left = pictureBox1.Left + 15;
+                pictureBox1.Image = Properties.Resources._3333;
+                sira += 1;
+            }
+            else if (sira == 4)
+            {
+                pictureBox1.Left = pictureBox1.Left + 15;
+                pictureBox1.Image = Properties.Resources._4444;
+                sira += 1;
+            }
+            else if (sira == 5)
+            {
+                pictureBox1.Left = pictureBox1.Left + 15;
+                pictureBox1.Image = Properties.Resources._5555;
+                sira = 1;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Animasyon";
+            this.BackColor = Color.Black;
+            pictureBox1.Location = new Point(0,0);
+            sira = 1;
+            timer1.Interval = 100;
+            timer2.Interval = 100;
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Left>=863)
+            {
+                pictureBox1.Left = -200;
+            }
+        }
+    }
+}
